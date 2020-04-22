83d0028fb80ba0fe55645e2ff7339acbc436ddab
Majid Jabrayilov <cmecid@gmail.com>
2020-04-23 00:23:17 +0400

    unused code removed

--- GithubSearch/Store.swift
+++ GithubSearch/Store.swift
@@ -11,43 +11,12 @@ import Combine
 typealias Reducer<State, Action, Environment> =
     (inout State, Action, Environment) -> AnyPublisher<Action, Never>?
 
-func lift<State, Action, Environment, LiftedState, LiftedAction, LiftedEnvironment>(
-    reducer: @escaping Reducer<LiftedState, LiftedAction, LiftedEnvironment>,
-    keyPath: WritableKeyPath<State, LiftedState>,
-    extractAction: @escaping (Action) -> LiftedAction?,
-    embedAction: @escaping (LiftedAction) -> Action,
-    extractEnvironment: @escaping (Environment) -> LiftedEnvironment
-) -> Reducer<State, Action, Environment> {
-    return { state, action, environment in
-        let environment = extractEnvironment(environment)
-        guard let action = extractAction(action) else {
-            return nil
-        }
-        let effect = reducer(&state[keyPath: keyPath], action, environment)
-        return effect.map { $0.map(embedAction).eraseToAnyPublisher() }
-    }
-}
-
-func combine<State, Action, Environment>(
-    _ reducers: Reducer<State, Action, Environment>...
-) -> Reducer<State, Action, Environment> {
-    return { state, action, environment -> AnyPublisher<Action, Never>? in
-        let effects = reducers.compactMap { $0(&state, action, environment) }
-        return Publishers
-            .Sequence(sequence: effects)
-            .flatMap { $0 }
-            .eraseToAnyPublisher()
-    }
-}
-
 final class Store<State, Action, Environment>: ObservableObject {
     @Published private(set) var state: State
-    
-    private let reducer: Reducer<State, Action, Environment>
-    private let environment: Environment
 
+    private let environment: Environment
+    private let reducer: Reducer<State, Action, Environment>
     private var effectCancellables: Set<AnyCancellable> = []
-    private var projectionCancellable: AnyCancellable?
 
     init(
         initialState: State,
@@ -64,53 +33,9 @@ final class Store<State, Action, Environment>: ObservableObject {
             return
         }
 
-        var didComplete = false
-        var cancellable: AnyCancellable?
-
-        cancellable = effect
+        effect
             .receive(on: DispatchQueue.main)
-            .sink(
-                receiveCompletion: { [weak self, weak cancellable] _ in
-                    didComplete = true
-                    cancellable.map { self?.effectCancellables.remove($0) }
-                }, receiveValue: { [weak self] in self?.send($0) })
-        if !didComplete, let cancellable = cancellable {
-            effectCancellables.insert(cancellable)
-        }
-    }
-
-    func projection<ProjectedState: Equatable, ProjectedAction>(
-        projectState: @escaping (State) -> ProjectedState,
-        projectAction: @escaping (ProjectedAction) -> Action
-    ) -> Store<ProjectedState, ProjectedAction, Void> {
-        let store = Store<ProjectedState, ProjectedAction, Void>(
-            initialState: projectState(state),
-            reducer: { _, action, _ in
-                self.send(projectAction(action))
-                return nil
-        },
-            environment: ()
-        )
-
-        store.projectionCancellable = $state
-            .map(projectState)
-            .removeDuplicates()
-            .sink { [weak store] in store?.state = $0 }
-
-        return store
-    }
-}
-
-import SwiftUI
-
-extension Store {
-    func binding<Value>(
-        for keyPath: KeyPath<State, Value>,
-        toAction: @escaping (Value) -> Action
-    ) -> Binding<Value> {
-        Binding<Value>(
-            get: { self.state[keyPath: keyPath] },
-            set: { self.send(toAction($0)) }
-        )
+            .sink(receiveValue: send)
+            .store(in: &effectCancellables)
     }
 }
